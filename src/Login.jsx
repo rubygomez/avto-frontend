f3280603a13b7a7d4dd330b9cd8a5a2f6a6e2566
rubygomez <rvbii.gomez@gmail.com>
2024-04-26 13:59:30 -0700

    render

--- src/Login.jsx
+++ src/Login.jsx
@@ -13,7 +13,7 @@ export function Login() {
         event.preventDefault();
         setErrors([]);
         const params = new FormData(event.target);
-        axios.post("http://localhost:3000/sessions.json", params).then((response) => {
+        axios.post("https://avto-backend.onrender.com/sessions.json", params).then((response) => {
             console.log(response.data);
             axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
             localStorage.setItem("jwt", response.data.jwt);
