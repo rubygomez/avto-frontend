30f7f71377185cc8540a21017fa8a8ca66b6592c
rubygomez <rvbii.gomez@gmail.com>
2024-05-25 01:30:27 -0700

    fixed footer positioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
-import { BrowserRouter } from "react-router-dom";
 
 
 function App() {
